55c0590506785b3b3b409caf717a9c9db25e0cee
Marco <ma.buracchi@gmail.com>
2018-08-22 14:31:17 +0200

    Esperimento

--- code/spark.c
+++ code/spark.c
@@ -108,11 +108,11 @@ int main(int argn, char *argv[]) {
 		}
 
 		int rangeMax =
-				((index + 1) * delta + (precisionLoss * delta) >= SIZE) ?
-						SIZE : (index + 1) * delta + (precisionLoss * delta);
-		int rangeMin =
-				((index - 1) * delta - (precisionLoss * delta) <= 0) ?
-						0 : (index - 1) * delta - (precisionLoss * delta);
+						((index + 1 + precisionLoss) * delta >= SIZE) ?
+								SIZE : (index + 1 + precisionLoss) * delta;
+				int rangeMin =
+						((index - 1 + precisionLoss) * delta <= 0) ?
+								0 : (index - 1 + precisionLoss) * delta;
 
 		if (results[index] > 0 && rangeMin <= secret[userUnderAttack]
 				&& rangeMax >= secret[userUnderAttack]) {
